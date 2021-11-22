2aae27af566b860fdbb80a4ac5caefdb9d738579
Jules Kho <juleskho@MacBook-Pro.lan>
2021-11-22 15:30:27 -0500

    undoing a mess

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { Component } from "react";
 import Home from './Pages/Home.js';
 import './App.scss';
 
-class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = { apiResponse: "" };
-  }
+function App() {
 
-  callAPI() {
-      fetch("http://localhost:3000/")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }))
-          .catch(err => err);
-  }
-
-  componentDidMount() {
-      this.callAPI();
-  }
-
-  render() {
-    return ( 
-      <Router>
-        <div className='App bg-image'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <div className='App bg-image'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
