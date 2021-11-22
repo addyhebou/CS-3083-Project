a44126909e505d3c9e6210129170e637b710783c
Jules Kho <juleskho@MacBook-Pro.lan>
2021-11-22 14:45:54 -0500

    initial backend files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Component } from "react";
 import Home from './Pages/Home.js';
 import './App.scss';
 
-function App() {
-  return (
-    <Router>
-      <div className='App bg-image'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:4000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render() {
+    return ( 
+      <Router>
+        <div className='App bg-image'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <p> API CALL: {this.state.apiResponse} </p>
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
